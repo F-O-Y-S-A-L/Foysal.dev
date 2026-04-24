f2f220980817b2c67126302939a8aa4bf95580b6
Foysal <foysal@example.com>
2026-04-24 22:04:50 +0600

    Remove .next from repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6orHLGVZqrQT9hI+q4XMQtTWfKnuNfohF1lNeUcCRyU=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_0kcl1js._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_039f.wl._.js","static/chunks/node_modules_0mgdzw-._.js"],
-    source: "dynamic"
-});
